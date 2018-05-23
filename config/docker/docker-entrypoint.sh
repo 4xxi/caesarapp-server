2a95228fefca87337bf363075e1828c30b8833a5
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2018-05-23 17:05:21 +0300

    Dockerize the application.

--- config/docker/docker-entrypoint.sh
+++ config/docker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$APP_ENV" = 'prod' ]; then
-    composer install --prefer-dist --no-dev --no-progress --no-suggest --optimize-autoloader --classmap-authoritative --no-interaction
+    composer auto-scripts --no-interaction
 else
     composer install --prefer-dist --no-progress --no-suggest --no-interaction
 fi
